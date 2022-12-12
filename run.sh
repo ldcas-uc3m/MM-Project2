2ce82adbd4f5075cc3776430599cc438303eae89
ldcas-uc3m <100429021@alumnos.uc3m.es>
2022-12-12 17:38:38 +0100

    query results + final touches

--- run.sh
+++ run.sh
@@ -2,28 +2,28 @@
 curl -XPUT "localhost:9200/multimedia?pretty" -H "Content-Type: application/json" -d @scripts/mappings.json
 
 # test one
-# curl -s -XPOST "localhost:9200/multimedia/_doc/test1?pretty" -H "Content-Type: application/json" -d @data/test.json
+curl -s -XPOST "localhost:9200/multimedia/_doc/test1?pretty" -H "Content-Type: application/json" -d @data/test.json
 # the id of the document was put as test1, if empty it's set as random
-# curl -s -XDELETE "localhost:9200/multimedia/_doc/test1?pretty"
+curl -s -XDELETE "localhost:9200/multimedia/_doc/test1?pretty"
 
 # upload documents
 curl -s -XPOST "localhost:9200/multimedia/_doc/_bulk?pretty" -H "Content-Type: application/json"  --data-binary @data/bulk_data
 
 # query1
 # World	War II films produced from 1980 onwards
-curl -XGET "localhost:9200/multimedia/_search?pretty" -H 'Content-Type: application/json' -d @scripts/query1.json
+curl -XGET "localhost:9200/multimedia/_search?pretty" -H 'Content-Type: application/json' -d @scripts/query1.json > results/query1.json
 
 # query2
 # Directors	with the most action films
-curl -XGET "localhost:9200/multimedia/_search?pretty" -H 'Content-Type: application/json' -d @scripts/query2.json
+curl -XGET "localhost:9200/multimedia/_search?pretty" -H 'Content-Type: application/json' -d @scripts/query2.json > results/query2.json
 
 # query3
 # LGBTQ-themed films available in the collection. In addition to the listing, show the number of films by year
-curl -XGET "localhost:9200/multimedia/_search?pretty" -H 'Content-Type: application/json' -d @scripts/query3.json
+curl -XGET "localhost:9200/multimedia/_search?pretty" -H 'Content-Type: application/json' -d @scripts/query3.json > results/query3.json
 
 # query4
-# LGBTQ-themed films available in the collection. In addition to the listing, show the number of films by year
-curl -XGET "localhost:9200/multimedia/_search?pretty" -H 'Content-Type: application/json' -d @scripts/query3.json
+# Films dealing with corrupt politicians in Europe and the United States
+curl -XGET "localhost:9200/multimedia/_search?pretty" -H 'Content-Type: application/json' -d @scripts/query3.json > results/query4.json
 
 # delete index
 curl -XDELETE "localhost:9200/multimedia?pretty"
